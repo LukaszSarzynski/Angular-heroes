9a00286d0e92f2133968bb3799648c7870caca0d
LukaszSarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-11-28 21:14:59 +0100

    revert ad829da A. starter - product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductAlertComponent } from './product-alert/product-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductsListComponent,
-    ProductAlertComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductsListComponent },
-    ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
