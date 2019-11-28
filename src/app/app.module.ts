3a7f5f675f135e65d1a24b17443afa20498ed4cd
LukaszSarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-11-28 21:10:59 +0100

    Revert "Angular Starer - add product details (RouterMod..)"
    This reverts commit 9c468c3bb9d1748c0a7e549907adbf3c7f2d97c9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductAlertComponent,
-    ProductDetailsComponent
+    ProductAlertComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: ProductsListComponent },
-      { path: 'product/:productId', component: ProductDetailsComponent },
     ])
   ],
   providers: [],
